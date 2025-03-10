cb3a38db340ec327bdd6967c9987b2dad15d3c45
Pavel Gladkikh <pavel.gladkikh@dsr-corporation.com>
2025-03-10 13:30:31 +0300

    [Tutorial Task] Rewrite `Board` to use two loops to make the squares instead of hardcoding them. (is `map` considered loop in this context?)

--- src/App.tsx
+++ src/App.tsx
@@ -36,24 +36,20 @@ function Board({xIsNext, squares, onPlay}: {
         status = 'Next player: ' + (xIsNext ? 'X' : 'O');
     }
 
+    const squareElements = squares.map((square: SquareValue, index: number) => (
+        <Square key={index} value={square} onSquareClick={() => handleClick(index)} />));
+
+    const size = 3;
+    const boardRows = [...Array(size).keys()].map((i) => (
+        <div key={i} className="board-row">
+            {squareElements.slice(size * i, size * (i + 1))}
+        </div>
+    ));
+
     return (
         <>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-            </div>
+            {boardRows}
         </>
     );
 }
